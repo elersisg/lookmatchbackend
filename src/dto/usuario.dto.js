c86cd06c30638399e59300126c060a6eeda16920
Gabriel <elersisgabriel@gmail.com>
2025-04-21 02:19:32 -0400

    tryagain

--- src/dto/usuario.dto.js
+++ src/dto/usuario.dto.js
@@ -56,6 +56,5 @@ module.exports = { RegistrarUsuarioDTO,
   VerificarContrasenaDTO, 
   ActualizarContrasenaDTO, 
   EliminarUsuarioDTO, 
-  RestablecerContrasenaDTO,
-  PerfilUsuarioResponseDTO 
+  RestablecerContrasenaDTO 
 };
