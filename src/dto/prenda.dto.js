3108c15da836de64fe54a3fb902154edda662c2a
Gabriel <elersisgabriel@gmail.com>
2025-04-23 02:36:17 -0400

    prenda issue

--- src/dto/prenda.dto.js
+++ src/dto/prenda.dto.js
@@ -9,16 +9,28 @@ class RegistrarPrendaDTO {
     color_secundario = null,
     ruta = '/uploads/default.jpg'
   }) {
+    const estilosValidos = ['Semiformal', 'Casual', 'Formal', 'Deportivo', 'Moderno'];
+      if (!estilosValidos.includes(estilo)) {
+      throw new Error(`Estilo inválido. Debe ser uno de: ${estilosValidos.join(', ')}`);
+    }
+    // Asegúrate de que coincidan con el enum
+    const estiloLimpio = estilo?.toString().trim().replace(/["']/g, ''); // elimina comillas
+
+    if (!estilosValidos.includes(estiloLimpio)) {
+      throw new Error(`Estilo inválido: "${estiloLimpio}". Debe ser uno de: ${estilosValidos.join(', ')}`);
+    }
+
     this.id_usuario = id_usuario;
     this.nombre_prenda = nombre_prenda;
     this.nombre_categoria = nombre_categoria;
     this.nombre_subcategoria = nombre_subcategoria;
-    this.estilo = estilo;
+    this.estilo = estiloLimpio;
     this.color_principal = color_principal;
     this.color_secundario = color_secundario;
-    this.ruta = ruta;  
+    this.ruta = ruta;
   }
 }
+
 
 class EditarPrendaDTO {
   constructor({
